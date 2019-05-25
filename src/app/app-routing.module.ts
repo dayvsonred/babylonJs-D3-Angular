c5fbddabb4d9abf51a0d04a023cfbb0f0b4e989d
Dayvson <dayvson.red@gmail.com>
2019-05-25 10:20:50 -0300

    ini angular babylon d3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PatioSimplesComponent } from './patio-simples/patio-simples.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'patio', pathMatch: 'full', component: PatioSimplesComponent  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/patio-simples/patio-simples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patio-simples',
+  templateUrl: './patio-simples.component.html',
+  styleUrls: ['./patio-simples.component.css']
+})
+export class PatioSimplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
