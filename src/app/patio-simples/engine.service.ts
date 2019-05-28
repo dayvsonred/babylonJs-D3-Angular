0770869a7399f671df34d33feddfbd92a4badd61
Dayvson <dayvson.red@gmail.com>
2019-05-27 23:40:46 -0300

    add movimento ponte

--- src/app/patio-simples/engine.service.ts
+++ src/app/patio-simples/engine.service.ts
@@ -15,6 +15,11 @@ export class EngineService {
 
   public sphere: BABYLON.Mesh;
   public terra: BABYLON.Mesh;
+  public corAmarelo : any;
+  public corAzul : any;
+  public corVerde : any;
+  public corVermelho : any;
+  public corRoxo : any;
 
   constructor() {
   }
@@ -44,9 +49,16 @@ export class EngineService {
 
     // Positions the camera overwriting alpha, beta, radius
     this.camera.setPosition(new BABYLON.Vector3((Math.PI / 2) *25 , (Math.PI /2) *25, 40));
+    
 
     // This attaches the camera to the canvas
-    this.camera.attachControl(this.canvas, true);
+    this.camera.attachControl(this.canvas, false);
+
+
+    //seta limite da terra para camera nao passar para baixo da terra
+    this.camera.lowerBetaLimit = 0.1;
+    this.camera.upperBetaLimit = (Math.PI / 2) * 0.99;
+    this.camera.lowerRadiusLimit = 30; //zoom limite
 
 
 
@@ -68,6 +80,7 @@ export class EngineService {
     let spherMaterial = new BABYLON.StandardMaterial('sun_surface', this.scene);
     spherMaterial.diffuseTexture = new BABYLON.Texture('assets/textures/sun.jpg', this.scene);
     this.sphere.material = spherMaterial;
+    this.corAmarelo = spherMaterial;
 
     // move the sphere upward 1/2 of its height
     this.sphere.position.y = 1;
@@ -97,11 +110,47 @@ export class EngineService {
 
     // this.Texturas();
 
+    // this.cores();
     this.Terra();
-
+ 
+    
+    
+    this.CriarCilindrosPosIni(0,0);this.CriarCilindrosPosIni(1,0);this.CriarCilindrosPosIni(2,0);this.CriarCilindrosPosIni(3,0);
+    this.CriarCilindrosPosIni(0,1);this.CriarCilindrosPosIni(1,1);this.CriarCilindrosPosIni(2,1);this.CriarCilindrosPosIni(3,1);
+    this.CriarCilindrosPosIni(0,2);this.CriarCilindrosPosIni(1,2);this.CriarCilindrosPosIni(2,2);this.CriarCilindrosPosIni(3,2);
+    this.CriarCilindrosPosIni(0,3);this.CriarCilindrosPosIni(1,3);this.CriarCilindrosPosIni(2,3);this.CriarCilindrosPosIni(3,3);
     
 
+    for (let index = 6; index < 11; index++) {
+      for (let index_l = 0; index_l < 4; index_l++) { 
+        this.CriarCilindrosPosIni(index,index_l);
+      }
+    }
 
+
+    for (let index = 12; index < 15; index++) {
+      for (let index_l = 0; index_l < 4; index_l++) { 
+        this.CriarCilindrosPosIni(index,index_l);
+      }
+    }
+
+
+    for (let index = 7; index < 11; index++) {
+      for (let index_l = 0; index_l < 5; index_l++) { 
+        this.CriarCilindrosPosIni(index_l,index);
+      }
+    }
+
+
+    for (let index = 12; index < 16; index++) {
+      for (let index_l = 0; index_l < 5; index_l++) { 
+        this.CriarCilindrosPosIni(index_l,index);
+      }
+    }
+    
+    
+    // let ponte  = this.Ponte();
+    this.ExeIniAnimations();
 
   }
 
@@ -132,6 +181,44 @@ export class EngineService {
   //   }  
 
 
+  ExeIniAnimations(){
+
+    let ponte  = this.Ponte();
+    
+
+    // setTimeout(function(){  this.ExeIniAnimations(ponte); }, 4000);
+    setTimeout(async () => {this.AnimPonte_Mov(ponte);}, 4000);
+    
+    //   setTimeout(async () => {
+    //     // var anim = this.scene.beginAnimation(box1, 0, 200, false);
+    
+    //     // console.log("before");
+    //     // await anim.waitAsync();
+    //     // console.log("after");
+    // });
+  }
+
+
+  cores(){
+    var vermelho = new BABYLON.StandardMaterial("redMat", this.scene);
+    vermelho.emissiveColor = new BABYLON.Color3(1,0,0);
+ 
+    var verde = new BABYLON.StandardMaterial("verdeMat", this.scene);
+    verde.emissiveColor = new BABYLON.Color3(0,1,0);
+ 
+    var verde2 = new BABYLON.StandardMaterial("verde2Mat", this.scene);
+    verde2.emissiveColor = new BABYLON.Color3((57/255),(203/255),(64/255));
+ 
+    var azul = new BABYLON.StandardMaterial("azulMat", this.scene);
+    azul.emissiveColor = new BABYLON.Color3((15/255),(13/255),(243/255));
+ 
+    this.corAmarelo = new BABYLON.StandardMaterial("amareloMat", this.scene);
+    this.corAmarelo.emissiveColor = new BABYLON.Color3((210/255),(181/255),0);
+ 
+ 
+    var preto = new BABYLON.StandardMaterial("pretoMat", this.scene);
+    preto.emissiveColor = new BABYLON.Color3((18/255),(10/255),(44/255));
+  }
 
 
 
@@ -225,12 +312,153 @@ export class EngineService {
     texturTerrono.diffuseTexture.wrapV = 6;
     texturTerrono.specularColor = new BABYLON.Color3(0, 0, 0)
 
-    let ground = BABYLON.Mesh.CreateGround('ground1',   63, 50,  2, this.scene);
+    let ground = BABYLON.Mesh.CreateGround('ground1',   80, 120,  2, this.scene);
     //   //ground.material = cor2;
       ground.material = texturTerrono;
+ 
+  }
 
 
-    	
+  Ponte(){
+
+    let ponte = BABYLON.Mesh.CreateBox('PontBase', 3,  this.scene);
+    ponte.position.x = 1;
+    ponte.position.y = 7;
+    ponte.position.z = 0;
+    ponte.material = this.corAmarelo;
+
+    return ponte;
+    // coluna4.material = amarelo;
+
+
+
+  }
+
+  /** Move a ponte para uma determinada posicao 
+   * 
+  */
+  AnimPonte_Mov(i){
+
+     //Create a scaling animation at 30 FPS
+    var animationBox = new BABYLON.Animation("PonteAnimation", "position.x", 100, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+                                                                    BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+    // Animation keys
+    var keys = [];
+    //At the animation key 0, the value of scaling is "1"
+    keys.push({
+        frame: 0,
+        value: 1
+    });
+
+    //At the animation key 20, the value of scaling is "0.2"
+    keys.push({
+        frame: 50,
+        value: -15
+    });
+
+    //At the animation key 100, the value of scaling is "1"
+    keys.push({
+        frame: 100,
+        value: -30
+    });
+
+    animationBox.setKeys(keys); 
+
+
+
+    var animationBox2 = new BABYLON.Animation("PonteAnimation2", "position.z", 100, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+    // Animation keys
+    var keys2 = [];
+    keys2.push({ frame: 0,value: 0 });
+    keys2.push({   frame: 50,  value: 15 }); 
+    keys2.push({ frame: 100, value: 30 });
+
+    animationBox2.setKeys(keys2);
+
+    i.animations.push(animationBox);
+    i.animations.push(animationBox2); 
+
+
+
+
+
+
+    // var anim = this.scene.beginAnimation(i, 0, 200, false);
+
+    // console.log("before");
+    // await anim.waitAsync();
+    // console.log("after");
+
+    
+    setTimeout(async () => {
+      var anim = this.scene.beginAnimation(i, 0, 100, false); 
+      
+
+      console.log("before");
+      await anim.waitAsync();
+      // i.position.x = -30;
+      // this.AnimPonte_Mov_2(i);
+      console.log("after");
+    });
+
+  }
+
+
+
+  AnimPonte_Mov_2(i){
+
+   var animationBox = new BABYLON.Animation("PonteAnimation2", "position.z", 100, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+                                                                   BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+   // Animation keys
+   var keys = [];
+   keys.push({ frame: 0,value: 0 });
+   keys.push({   frame: 50,  value: 15 }); 
+   keys.push({ frame: 100, value: 30 });
+
+   animationBox.setKeys(keys);
+
+   i.animations.push(animationBox);
+
+   var anim = this.scene.beginAnimation(i, 0, 100, false);
+
+   // console.log("before");
+   // await anim.waitAsync();
+   // console.log("after");
+
+ }
+
+
+
+
+
+  /**
+   * cria cilindros na posição inicial
+   * @param Coluna coluna da matrix number ini 0
+   * @param Linha linha da matrix
+   */
+  CriarCilindrosPosIni(Coluna, Linha){
+
+    let posX = 35; // move para esqueda direita
+    let posY = 1; // move para cima e baixo
+    let posZ = 40; // move para traz e fente
+    let NameCilindre = "cylindre_"+Coluna+"_"+Linha;
+
+
+    let MargenColunas = 4;
+    let MargenLinhas = 4;
+
+    posX = (posX - (MargenColunas * Coluna))*-1;
+    posZ = (posZ - (MargenLinhas * Linha));
+
+
+    let cylindro1 = BABYLON.Mesh.CreateCylinder(NameCilindre, 2, 2, 2, 20, 6 , this.scene)
+    cylindro1.rotation.x = (Math.PI * 90) /180;
+    cylindro1.position.x = posX;
+    cylindro1.position.y = posY;
+    cylindro1.position.z = posZ;
+
+
 
 
   }
